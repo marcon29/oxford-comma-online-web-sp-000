cde6f999b85c4ba1daaf37b2695221100b5e705b
Mark Stabler <mark@stablerwritingservices.com>
2020-03-04 20:11:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.size
+  	when 1
+  		array.shift
+  	when 2
+  		array.shift << " and " << array.pop
+   	else
+      last = " and " << array.pop
+   		array.shift << ", " << array.join(", ") << "," << last
+  end
 end
