48a1ab456809d6c6655ae565ee89c1b4985f9b35
Mark Stabler <mark@stablerwritingservices.com>
2020-03-04 19:33:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  first = array.shift
+  last = " and " << array.pop
+  middle = ", " << array.join(", ") << ","
 end
+
+first << middle << last
+
+# array = ["kiwi"]
+# array = ["kiwi", "durian"]
+array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
