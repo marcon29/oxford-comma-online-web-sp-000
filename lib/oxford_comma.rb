7ae73ce87dd764fa4d5cc47cfd25bee3ee671c5a
Mark Stabler <mark@stablerwritingservices.com>
2020-03-04 19:52:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,28 @@
 def oxford_comma(array)
-  first = array.shift
-  last = " and " << array.pop
-  middle = ", " << array.join(", ") << ","
+  case array.size
+  	when == 1
+  		array.shift
+  	when == 2
+  		array.shift << " and " << array.pop
+  	when > 2
+  		last = " and " << array.pop
+  		array.shift << ", " << array.join(", ") << "," << last
+  end
 end
 
-first << middle << last
+# first = array.shift
+# last = " and " << array.pop
+# middle = ", " << array.join(", ") << ","
+# first << middle << last
 
-array.reject do |item|
-  <code to execute using arg>
-end
+# middle = []
+# array.each_with_index do |item, index|
+#   middle.push(array[index+1])
+# end
+# middle.pop
+# middle
 
 # array = ["kiwi"]
 # array = ["kiwi", "durian"]
 array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
+puts oxford_comma(array)
